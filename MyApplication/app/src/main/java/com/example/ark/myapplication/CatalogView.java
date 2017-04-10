75600bdbfca3a047e6c2406cd28355cae8ff1360
hthkwan <herman_fire@hotmail.com>
2017-04-10 17:22:07 -0600

    building catalog list to be tested

--- MyApplication/app/src/main/java/com/example/ark/myapplication/CatalogView.java
+++ MyApplication/app/src/main/java/com/example/ark/myapplication/CatalogView.java
@@ -2,9 +2,14 @@ package com.example.ark.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CatalogView extends AppCompatActivity {
 
@@ -17,19 +22,56 @@ public class CatalogView extends AppCompatActivity {
 
         String labelText = "";
 
+        List<? extends Products> list = null;
+
         if (type.equals("Liquor")){
             labelText = "Liquor";
+            try {
+                list = MainActivity.db.getAllLiquors();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }else if(type.equals("Non-Liquor")){
             labelText = "Non-Liquor";
+            try {
+                list = MainActivity.db.getAllNonLiquors();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }else if(type.equals("Misc")){
             labelText = "Misc.";
-        }
+            try {
+                list = MainActivity.db.getAllMiscs();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("wrong args passed to CatalogView");        }
 
         TextView typeLabel = (TextView)findViewById(R.id.TypeLabel);
         typeLabel.setText(labelText);
 
-        CatalogActivity temp = new CatalogActivity(this);
-        
+        //TEST LIST
+        List<Liquor> testlist = new ArrayList<Liquor>();
+        for (int i = 0; i < 10; i++){
+            Liquor i1 = new Liquor();
+            i1.setConcentration("5%");
+            i1.setLtype("Beer");
+            i1.setVolume("750 mL");
+            testlist.add(i1);
+        }
+
+        //fill the catalog with items returned from db query
+        LinearLayout catalogList = (LinearLayout)findViewById(R.id.catalogLayout);
+
+        for(Products p : list){
+            CatalogActivity temp = new CatalogActivity(this);
+            temp.setName(p);
+            temp.setBrand(p);
+            temp.setPrice(p);
+
+            catalogList.addView(temp);
+        }
 
 
     }
